72b780185630814f96e9b090f68efc0781eca5dc
constkashyap <sanchitkshyap@gmail.com>
2021-06-01 12:06:54 +0530

    id

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ def gh_pull():
 @app.route('/push_req', methods=['POST'])
 def gh_push():
     if request.headers["Content-Type"] == 'application/json':
-        print((request.json['commits'][0]))
+        print((request.json['commits'][0]['id']))
         data = dict(
             request_id = request.json.commits['id'],
             author = request.json["pusher"]['name'],
